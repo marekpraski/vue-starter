d4058c894fe2dbd342d4ce7cdde5716474db6334
student15 <student15@pcoip.ki.agh.edu.pl>
2019-04-27 12:19:11 +0200

    my commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div H1>
+	Witaj w systemie zapisów
+	<p>{{email}}</p>
+	<!--  <div H2>{{email}}</div> -->
+	<!--  <div H2>{{password}}</div> -->
+
+
+
+	<button @click="logMeIn">Wchodzę</button>
+	<div v-if="isAuthenticated">
+		Zalogowany jako {{email}} <a  @click="logMeOut()">wyloguj mnie</a>
+	</div>
+	<div v-else>
+		<login-form @login="enter($event)" :button-label="'Wejdź'"></login-form>
+
+	</div>
+
+	<div
+		<!--  <div v-if="email.length < 10">Ale masz krótki adres!</div>
+<div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+<div v-else>Twój adres e-mail jest stanowczo za długi.</div> -->
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import "milligram";
+import LoginForm from "./LoginForm";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+components: {LoginForm},
+ data() {
+	 return {
+		 email: '',
+		 isAuthenticated: false,
+		// password: "krótkie hasło"
+	 }
+ },
+ methods: {
+	 alertMyEmail() {
+		    alert(this.email);
+		  },
+	logMeIn() {
+			  this.isAuthenticated=true;
+			  
+		  },
+		  logMeOut(){
+			 this.isAuthenticated=false;
+			 this.email='';
+		  },
+		  enter() {
+			  this.$emit('login',this.email);
+		  }
+		 
+ }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1 {
+	font-size: 10px
 }
 </style>
